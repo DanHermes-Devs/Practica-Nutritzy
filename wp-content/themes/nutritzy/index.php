ffdaef39011e5e4b291cc4b36eaae01bcdf84ea5
Dan Hermes <80308086+DanHermes-Devs@users.noreply.github.com>
2021-09-13 10:48:55 -0500

    Summary

--- wp-content/themes/nutritzy/index.php
+++ wp-content/themes/nutritzy/index.php
@@ -1,4 +1,4 @@
 <?php get_header(); ?>
 <h1>Desde index</h1>
 <?php get_footer(); ?>
-    
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, consequuntur consectetur. Incidunt, dolorem repellendus veniam hic, blanditiis alias mollitia, maiores fuga voluptate deleniti nisi minus saepe quisquam id itaque ut.
